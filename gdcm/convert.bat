ef39cce0673df89008e3faf4f3a2361bde9ee47b
ke-senthilkumar <kesenthilkumar@outlook.com>
2025-10-31 12:49:53 +0530

    Refactor convert.bat to preserve directory structure
    The script now processes all files in the input directory recursively, preserving the directory structure in the output.

--- gdcm/convert.bat
+++ gdcm/convert.bat
@@ -1,3 +1,26 @@
-for /R E:\in\ %%i IN (*.dcm) DO (
-gdcmconv -w -i  %%i -o E:\out\%%~nxi
+@echo off
+setlocal enabledelayedexpansion
+
+set "IN_DIR=d:\in"
+set "OUT_DIR=d:\out"
+
+for /R "%IN_DIR%" %%i in (*.*) do (
+    :: Get full input file path
+    set "INFILE=%%i"
+
+    :: Build relative path (remove input base path)
+    set "REL_PATH=!INFILE:%IN_DIR%=!"
+
+    :: Build output file path with same subdirectory structure
+    set "OUTFILE=%OUT_DIR%!REL_PATH!"
+
+    :: Make sure destination folder exists
+    for %%D in ("!OUTFILE!") do if not exist "%%~dpD" mkdir "%%~dpD"
+
+    :: Process file with gdcmconv, preserving structure
+    echo Processing "%%i"
+    gdcmconv -r -w -i "%%i" -o "!OUTFILE!"
 )
+
+echo Done!
+pause 
